828ff4e5ed15f9528fa799868d9525f1e1dc3085
kutomeieiei <kutomei38@gmail.com>
2026-05-28 18:39:29 +0700

    style: adjust z-index and shadowing for landing sections
    Increase z-index values to ensure proper layering of page sections and add white shadows to the middle section for improved visual depth.

--- src/App.tsx
+++ src/App.tsx
@@ -359,7 +359,7 @@ export default function App() {
               className="flex flex-col items-center justify-start w-[100vw] relative left-1/2 -translate-x-1/2 -mt-8 min-h-[70vh]"
             >
               {/* Top Section */}
-              <div className="w-[100vw] relative left-1/2 -translate-x-1/2 bg-neutral-50 dark:bg-[#050505] text-neutral-900 dark:text-white pt-24 pb-8 flex flex-col items-center z-20">
+              <div className="w-[100vw] relative left-1/2 -translate-x-1/2 bg-neutral-50 dark:bg-[#050505] text-neutral-900 dark:text-white pt-24 pb-8 flex flex-col items-center z-[40]">
                 {/* Ambient Background Glow Container */}
                 <div className="absolute inset-0 overflow-visible pointer-events-none z-[25]">
                   <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[350px] bg-red-500/10 dark:bg-red-900/15 rounded-full blur-[100px]"></div>
@@ -380,9 +380,9 @@ export default function App() {
               </div>
 
               {/* Middle Section */}
-              <div className="w-[100vw] relative left-1/2 -translate-x-1/2 bg-white dark:bg-neutral-900 pt-6 pb-4 z-10 overflow-visible">
+              <div className="w-[100vw] relative left-1/2 -translate-x-1/2 bg-white dark:bg-white shadow-[0_0_60px_20px_rgba(255,255,255,1)] dark:shadow-[0_0_100px_30px_rgba(255,255,255,0.5)] pt-6 pb-4 z-[35] overflow-visible">
                 {/* SVG dripping shape at the top of Middle Section */}
-                <div className="absolute top-0 left-0 w-[100vw] overflow-hidden leading-[0] z-20 drop-shadow-xl pointer-events-none">
+                <div className="absolute top-0 left-0 w-[100vw] overflow-hidden leading-[0] z-20 drop-shadow-[0_20px_30px_rgba(255,255,255,0.3)] pointer-events-none">
                   <svg className="block w-full h-[220px] md:h-[440px] fill-neutral-50 dark:fill-[#050505] drop-shadow-md" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M0,0 V46.29C39.4,46.29 55.6,90.3 113,90.3 170.4,90.3 186.6,18.4 226,18.4 265.4,18.4 286.7,71.2 339,71.2 391.3,71.2 411.5,12 452,12 492.5,12 506.7,55 565,55 623.3,55 640.7,21 678,21 715.3,21 732.1,80 791,80 849.9,80 862.6,35 904,35 945.4,35 965.7,85 1017,85 1068.3,85 1084.7,26 1130,26 1175.3,26 1186,46.29 1200,46.29V0Z"></path>
                   </svg>
@@ -393,13 +393,20 @@ export default function App() {
                     <ImageSlider />
                   </div>
                 </div>
+
+                {/* SVG Curve at the bottom of Middle Section */}
+                <div className="absolute top-full left-0 w-[100vw] leading-[0] z-20 pointer-events-none -mt-[1px]">
+                  <svg className="block w-full h-[60px] md:h-[100px] fill-white dark:fill-white" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                     <path d="M0,0 Q600,120 1200,0 Z"></path>
+                  </svg>
+                </div>
               </div>
 
-              {/* Smooth transition spacer right below the slider */}
-              <div className="w-[100vw] h-4 relative left-1/2 -translate-x-1/2 bg-white dark:bg-neutral-900 z-10 pointer-events-none" />
+              {/* Bottom transition spacer beneath the curve */}
+              <div className="w-[100vw] h-[80px] relative left-1/2 -translate-x-1/2 bg-neutral-50 dark:bg-[#050505] z-[30] pointer-events-none" />
 
               {/* Bottom Section */}
-              <div className="w-[100vw] relative left-1/2 -translate-x-1/2 bg-neutral-50 dark:bg-[#050505] text-neutral-900 dark:text-white pt-10 pb-24 mt-auto z-10 flex flex-col items-center">
+              <div className="w-[100vw] relative left-1/2 -translate-x-1/2 bg-neutral-50 dark:bg-[#050505] text-neutral-900 dark:text-white pt-10 pb-24 mt-auto z-[30] flex flex-col items-center">
                 {/* Ambient Background Glow Container */}
                 <div className="absolute inset-0 overflow-hidden pointer-events-none z-0">
                   <div className="absolute top-[10%] left-[-15%] w-[45%] h-[350px] bg-rose-500/10 dark:bg-rose-900/15 rounded-full blur-[110px]"></div>
